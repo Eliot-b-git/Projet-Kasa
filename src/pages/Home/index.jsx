cdab5b7dc63c45f38d7bb43c199ffee936a29258
Eliot B <99880596+Eliot-b-git@users.noreply.github.com>
2023-08-15 12:29:11 +0200

    Ajout du texte de la banniere home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banniere from '../../components/Banner'; // Assurez-vous d'utiliser le bon chemin
 import LogementsPage from '../../components/Card'
+import './styles.scss';
 
 function Home() {
   return (
     <div>
+      <p className='TexteBanniere'>Chez vous, partout et ailleurs</p>,
       <Banniere type="accueil" />
       <LogementsPage />
     </div>
